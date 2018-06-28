4a4ba941814529d4515f3bd87996ab175d650070
Andreea Onofriesei <bubuux9@gmail.com>
2018-06-28 15:21:45 +0300

    templating and bug fixes

---
+++ resources/views/partials/index_partials/_what_makes_us_different.blade.php
@@ -0,0 +1,26 @@
+<!-- WHAT MAKES US DIFFERENT -->
+<div>
+    <div class="row">
+        <div class="col-md-12">
+            <p class="why_us"> What makes us different ? </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 text-center">
+            <img src="{{asset('uploads/index/graduates.png')}}" class="why_us_img">
+            <p class="why_us_text"> All of our students gratuate, the rate of abandon is null.</p>
+        </div>
+        <div class="col-md-4 text-center">
+            <img src="{{asset('uploads/index/certificate.png')}}" class="why_us_img">
+            <p class="why_us_text"> Your diploma will be recognized everywhere on Earth.</p>
+        </div>
+        <div class="col-md-4 text-center">
+            <img src="{{asset('uploads/index/id-card.png')}}" class="why_us_img">
+            <p class="why_us_text"> By the time you finish your studies, you will already have work experience and a career granted.</p>
+        </div>
+    </div>
+    <hr>
+</div>
+
+<!-- / WHAT MAKES US DIFFERENT -->
