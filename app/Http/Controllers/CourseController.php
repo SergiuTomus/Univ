293f3a0f444e3e1a7c5d985c1869670b4e39b23e
ReeaPractica-PC\Reea Practica <dcristian35@yahoo.com>
2018-06-28 15:43:34 +0300

    doar test

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Illuminate\Routing\Controller as BaseController;
+
 
 class CourseController extends Controller
 {
--- app/Http/Controllers/HallsController.php
+++ app/Http/Controllers/HallsController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Illuminate\Routing\Controller as BaseController;
 
 class HallsController extends Controller
 {
   
 
-    public function halls(){
+   /* public function halls(){
         return view('pages.halls');
+    }*/
+
+    public function halls()
+    {
+    
+                return view('pages.halls');
+
     }
+
 
 }
 ?>
