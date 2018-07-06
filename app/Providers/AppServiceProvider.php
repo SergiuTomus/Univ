405cd10150317f70ac13d84ca681a78cab65ce10
Andreea Onofriesei <bubuux9@gmail.com>
2018-07-06 15:50:29 +0300

    cevaaaa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      //  $gate->define('create-app', function ($user, $appointment) {
-         //   return $user->id === $appointment->user_id;
+        //
     }
 
     /**
