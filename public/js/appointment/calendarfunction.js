4fb505164f1247194edbc3b722a23c70dace6b87
sergiutomus <sergiutomus@yahoo.com>
2018-07-09 12:08:23 +0300

    update modal

--- public/js/appointment/calendarfunction.js
+++ public/js/appointment/calendarfunction.js
@@ -7,27 +7,55 @@ $(document).ready(function() {
             success:function (response) {
                 if(response['userRole'] === 3) {
                   
-
+                  //var course;
                   $('#calendar').fullCalendar({
                     events: function(start, end, timezone, callback) {
+                      
                       $.ajax({
                         url: allAppointmentsRoute,
                         dataType: 'json',
                         success: function(response) {
-            
+
                         var events = [];
                         var length =  response['appointments'].length;
                         for(i=0; i<length; i++) {
                             events.push({
                                 title: response['appointments'][i]['hallName'],
                                 start: response['appointments'][i]['app_date'],
+                                end: response['appointments'][i]['end_date'],
+                                course: response['appointments'][i]['courseName'],
+                                app_id: response['appointments'][i]['id'],
                               });
                          }
             
                           callback(events);
                         }
                       });
-                    }
+                    },
+
+                    eventClick: function(calEvent, jsEvent, view) {
+
+                      $('#detailsModal').modal('show');
+                      $("h6").html('The scheduled course is: ' + calEvent.course);
+                      //alert('Event: ' + calEvent.start);
+                      //alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+                      //alert('View: ' + view.name);
+                  
+                      // change the border color just for fun
+                      $(this).css('border-color', 'red');
+                  
+                    },
+
+                    eventAfterRender: function(event, $el, view) {
+                      var formattedTime = $.fullCalendar.formatDates(event.start, event.end, "HH:mm { - HH:mm}");
+                      // If FullCalendar has removed the title div, then add the title to the time div like FullCalendar would do
+                      if($el.find(".fc-event-title").length === 0) {
+                          $el.find(".fc-event-time").text(formattedTime + " - " + event.title);
+                      }
+                      else {
+                          $el.find(".fc-event-time").text(formattedTime);
+                      }
+                  }
             
                 });
 
@@ -68,7 +96,20 @@ $(document).ready(function() {
             
                       //$(this).css('background-color', '#ccffff');
             
+                    },
+
+                    eventClick: function(calEvent, jsEvent, view) {
+
+                      $('#detailsModal').modal('show');
+                      //alert('Event: ' + calEvent.start);
+                      //alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+                      //alert('View: ' + view.name);
+                  
+                      // change the border color just for fun
+                      $(this).css('border-color', 'red');
+                  
                     }
+
                 });
 
                 $('#btn-sendData').click(function() {
